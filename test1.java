ffa36bcdb7e850ed7677d5ca596d6f1504644680
tako511 <72993010+tako511@users.noreply.github.com>
2020-10-18 01:34:40 +0800

    exercise1——九九乘法表
    思路：先确定a*b中a的值，使0<a<10，再给b赋值并且值小于a，利用两个for语句的嵌套循环计算a*b
    心得：for的嵌套语句其第一个for语句可控制输出行数，而其第二个for语句可以控制输出列数。利用转义字符'\t'使每两个输出结果间的距离相等。

--- test1.java
+++ test1.java
@@ -1,10 +1,16 @@
+package exercise;
+
 public class exercise {
 	public static void main(String[] args) {
-		for (int a = 9;a > 0 && a < 10;a--) {
-			for (int b = 1;b <= a;b++) {
-		    	System.out.print(a+"*"+b+"="+a*b+'\t');
+		for (int A = 9;A > 0 && A < 10;A--) {
+			//使A等于9，并以1为单位逐渐减小直到A等于1
+			for (int B = 1;B <= A;B++) {
+				//使B等于1，并以1为单位逐渐增加直到B等于A
+		    	System.out.print(A+"*"+B+"="+A*B+'\t');
+		    	//循环输出A*B=？的公式，利用'\t'使每两个输出值之间的距离相等
 			}
 			System.out.println();
+			//将A不同的输出值分隔开
 		}
 	}
 }
